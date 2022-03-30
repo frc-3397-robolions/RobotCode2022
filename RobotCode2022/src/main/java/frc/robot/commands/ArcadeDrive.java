c7408df56cb1c45b5f2d0d9bf10cb78482f40bb0
frc-3397-robolions <robolions3397@gmail.com>
2022-03-30 11:05:13 -0500

    add vision control to driving

--- RobotCode2022/src/main/java/frc/robot/commands/ArcadeDrive.java
+++ RobotCode2022/src/main/java/frc/robot/commands/ArcadeDrive.java
@@ -7,9 +7,30 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class ArcadeDrive extends Command {
+// Constants such as camera and target height stored. Change per robot and goal!
+    final double CAMERA_HEIGHT_METERS = Units.inchesToMeters(24);
+    final double TARGET_HEIGHT_METERS = Units.feetToMeters(5);
+    // Angle between horizontal and the camera.
+    final double CAMERA_PITCH_RADIANS = Units.degreesToRadians(0);
+
+    // How far from the target we want to be
+    final double GOAL_RANGE_METERS = Units.feetToMeters(3);
+
+    // Change this to match the name of your camera
+    PhotonCamera camera = new PhotonCamera("photonvision");
+
+    // PID constants should be tuned per robot
+    final double LINEAR_P = 0.1;
+    final double LINEAR_D = 0.0;
+    PIDController forwardController = new PIDController(LINEAR_P, 0, LINEAR_D);
+
+    final double ANGULAR_P = 0.1;
+    final double ANGULAR_D = 0.0;
+    PIDController turnController = new PIDController(ANGULAR_P, 0, ANGULAR_D);
   public ArcadeDrive() {
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
+    requires(driveTrain);
   }
 
   // Called just before this Command runs the first time
@@ -18,7 +39,42 @@ public class ArcadeDrive extends Command {
 
   // Called repeatedly when this Command is scheduled to run
   @Override
-  protected void execute() {}
+  protected void execute() {
+  double forwardSpeed;
+  double rotationSpeed;
+    if(oi.xbController.getYButton()){
+    var result = camera.getLatestResult();
+      if (result.hasTargets()) {
+                // First calculate range
+                double range =
+                        PhotonUtils.calculateDistanceToTargetMeters(
+                                CAMERA_HEIGHT_METERS,
+                                TARGET_HEIGHT_METERS,
+                                CAMERA_PITCH_RADIANS,
+                                Units.degreesToRadians(result.getBestTarget().getPitch()));
+
+                // Use this range as the measurement we give to the PID controller.
+                // -1.0 required to ensure positive PID controller effort _increases_ range
+                forwardSpeed = -forwardController.calculate(range, GOAL_RANGE_METERS);
+
+                // Also calculate angular power
+                // -1.0 required to ensure positive PID controller effort _increases_ yaw
+                rotationSpeed = -turnController.calculate(result.getBestTarget().getYaw(), 0);
+            }
+      else{
+                // If we have no targets, stay still.
+                forwardSpeed = 0;
+                rotationSpeed = 0;
+            }
+        
+    }
+    else{
+      forwardSpeed = oi.xbController.getStickY();
+      rotationSpeed = oi.xbController.getStickX();
+    }
+    Robot.driveTrain.setLeftMotors((forwardSpeed+rotationSpeed)*RobotMap.baseDriveSpeed);
+    Robot.driveTrain.setRightMotors((forwardSpeed-rotationSpeed)*RobotMap.baseDriveSpeed);
+  }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
